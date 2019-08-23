63ee298d394c771ea90583218deebebc86d6df91
Kyle Guerrero <kyleguerrero85@gmail.com>
2019-08-23 10:46:16 -0500

    Completed CharaterList and cards

--- src/App.js
+++ src/App.js
@@ -2,12 +2,26 @@ import React from "react";
 import TabNav from "./components/TabNav.js";
 import Header from "./components/Header.js";
 
+// Main components of the app
+import WelcomePage from './components/WelcomePage';
+import CharacterList from './components/CharacterList';
+
+// Styles
+import './index.css'
+
 
 export default function App() {
   return (
     <main>
       <Header />
-      {/* <TabNav /> */}
+			{/* <TabNav /> */}
+
+			{/* '/' Route */}
+			{/* <WelcomePage /> */}
+
+			{/* Characters View */}
+			<CharacterList />
+
     </main>
   );
 }
